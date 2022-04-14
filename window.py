fe0a966018aabfa7d73f36418bc47e4de54b76fd
Hecong Wang <hecongwang19991222@gmail.com>
2022-04-13 21:53:46 -0400

    Code formatting

--- window.py
+++ window.py
@@ -1,8 +1,12 @@
-from poss_ing_of import sentence_to_graph, in_edges, out_edges
+from __future__ import annotations
+
 from coca import Sentence
+from poss_ing_of import in_edges, out_edges, sentence_to_graph
 
 
 def window(sentence: Sentence, center: int) -> list[int]:
     dependencies = sentence.dependency.to_dict()[0]
     graph = sentence_to_graph(dependencies)
-    return [source for source, _, _ in in_edges(graph, center)] + [target for _, target, _ in out_edges(graph, center)]
+
+    return ([source for source, _, _ in in_edges(graph, center)] +
+            [target for _, target, _ in out_edges(graph, center)])
