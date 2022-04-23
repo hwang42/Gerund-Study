fe2f59a4235e6fe5afaa703a5d6746d9eb843119
Deepak S V <svdeepak99@users.noreply.github.com>
2022-04-23 12:08:10 -0400

    Added Tags, Dependencies & Relevant Dependencies Columns

--- example.py
+++ example.py
@@ -3,12 +3,12 @@ import pickle
 import pandas as pd
 import os, csv
 from poss_ing_of import poss_ing_of, sentence_iterator
-from window import window, longest_dep
+from window import window
 
 from tqdm import tqdm
 
 if __name__ == "__main__":
-    
+
     # takes one command line argument: directory of pickle files that contain the dependency parses
     parser = argparse.ArgumentParser()
     parser.add_argument("directory", help="Directory of the pickle files.")
@@ -19,19 +19,20 @@ if __name__ == "__main__":
     filenames = [os.path.join(args.directory, x) for x in filenames]
     # read sentence_id, word position, and exclusion result from pattern-matching output csv
     gerunds = pd.read_csv("updated-updated-updated-matches.csv", usecols=["id", "position", "exclusion"])
-    # create structure of final csv output with annotated gerund type added, and relevant dependencies
-    csv_output = [['Recommended Exclusion', 'Sentence ID', 'Position', 'Word', 'Gerund Type', 'Rel_Dep 1 Step', 'Rel_Dep Longest', 'Sentence']]
+    # create structure of final csv output with annotated gerund type added, tags and relevant dependencies
+    csv_output = [['Recommended Exclusion', 'Sentence ID', 'Position', 'Word', 'Gerund Type', 'Tags', 'Dependencies',
+                   'Relevant Dependencies', 'Sentence']]
 
     # iterate and load each pickle file
     for filename in tqdm(filenames):
         with open(filename, "rb") as file:
             text = pickle.load(file)
-        
+
         # iterate through all sentences in that pickle file
         for id, sentence in sentence_iterator(text):
             if sentence.skipped:
                 continue
-            
+
             # find all gerunds that have that sentence_id, and store in rslt_df dataframe
             rslt_df = gerunds.loc[gerunds['id'] == id]
             # extract all centers (indices of gerunds) from that sentence
@@ -40,17 +41,17 @@ if __name__ == "__main__":
             # for each center and its categorization (all centers+=1 because we switch from 0- to 1-indexed)
             for center, kind in poss_ing_of(sentence, [i + 1 for i in centers]):
                 # store excluded results
-                excluded = rslt_df.loc[(rslt_df['position'] == center-1), 'exclusion'].item()
+                excluded = rslt_df.loc[(rslt_df['position'] == center - 1), 'exclusion'].item()
                 # change from Na to a space
                 if pd.isna(excluded):
                     excluded = " "
-                rel_dep1 = window(sentence, center)
-                rel_dep2 = longest_dep(sentence, center)
+                tags, all_dep, rel_dep = window(sentence, center)
                 # add this gerund data to the output csv
-                csv_output.append([excluded, id, center, sentence.tokens[center - 1], kind, rel_dep1, rel_dep2, ' '.join(sentence.tokens)])
+                csv_output.append([excluded, id, center, sentence.tokens[center - 1], kind, tags, all_dep, rel_dep,
+                                   ' '.join(sentence.tokens)])
 
-    #TODO: add rule, two other columns to output
+    # TODO: add rule, two other columns to output
     # write the full output to a csv file
-    with open("Gerunds-5.csv", "w", newline='') as fp:
-            writer = csv.writer(fp)
-            writer.writerows(csv_output)
+    with open("Gerunds-6.csv", "w", newline='') as fp:
+        writer = csv.writer(fp)
+        writer.writerows(csv_output)
--- window.py
+++ window.py
@@ -4,23 +4,38 @@ from coca import Sentence
 from poss_ing_of import in_edges, out_edges, sentence_to_graph
 
 
-def window(sentence: Sentence, center: int) -> str:
+def window(sentence: Sentence, center: int) -> tuple[str, str, str]:
     dependencies = sentence.dependency.to_dict()[0]
     graph = sentence_to_graph(dependencies)
-    sen = sentence.tokens
+    # sen = sentence.tokens
 
-    win = [[sen[source-1] for source, _, _ in in_edges(graph, center)],
-            [sen[target-1] for _, target, _ in out_edges(graph, center)]]
+    relevant_indices = set([source for source, _, _ in in_edges(graph, center)] + [center] +
+                            [target for _, target, _ in out_edges(graph, center)])
 
-    return ', '.join(win[0]) + " -> " + sen[center-1] + " -> " + ', '.join(win[1])
+    tags = ' '.join([f"{x['text']}/{x['lemma']}/{x['xpos']}/{x['upos']}" for x in dependencies])
+    all_dep = []
+    rel_dep = []
+    for x in dependencies:
+        head = x['head']
+        if head == 0:
+            continue
+        parent = dependencies[head - 1]
+        dep = f"{x['deprel']} ({x['text']}-{x['id']}, {parent['text']}-{parent['id']})"
+        all_dep.append(dep)
+        if x['id'] in relevant_indices:
+            rel_dep.append(dep)
+    all_dep = ', '.join(all_dep)
+    rel_dep = ', '.join(rel_dep)
 
+    return tags, all_dep, rel_dep
 
+'''
 def longest_dep(sentence: Sentence, center: int) -> str:
     dependencies = sentence.dependency.to_dict()[0]
     graph = sentence_to_graph(dependencies)
     sen = sentence.tokens
 
-    p_node = [sen[source-1] for source, _, _ in in_edges(graph, center)]
+    p_node = [sen[source - 1] for source, _, _ in in_edges(graph, center)]
 
     def longest_branch(parent: int) -> list[str]:
         max_len = 0
@@ -30,6 +45,7 @@ def longest_dep(sentence: Sentence, center: int) -> str:
             if len(b) > max_len:
                 branch = b
                 max_len = len(b)
-        return [sen[parent-1]] + branch
+        return [sen[parent - 1]] + branch
 
     return " -> ".join(p_node + longest_branch(center))
+'''
